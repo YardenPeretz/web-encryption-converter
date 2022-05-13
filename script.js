24ebfd76067fd2326114344e7defc164a695dea5
YardenPeretz <44260060+YardenPeretz@users.noreply.github.com>
2022-05-13 18:27:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,21 @@ const selected_format = document.getElementById("selected_format");
 window.addEventListener("load",() => {
     // GET VALUE OF SELECTED FORMAT FROM INPUT.
     selected_format.addEventListener("change",() => {
-        console.log(selected_format.value);
+       // console.log(selected_format.value);
         const format = selected_format.value;
         // WRITE DECODING CODE HERE
-        switch(format){
-            case format == "base64":
-                console.log("base64");
-            case format == "sha256":
-                console.log("sha");
+        switch(selected_format.value){
+            case "base64":
+                 console.log("base64");
+                 break;
+            case "sha256":
+                 console.log("sha");
+                 break;
+            default:
+                console.log(selected_format.value)
         }
 
     });
 }); // END LOAD EVENT
+
 
